dda6bf389dcac33eb7ce133d8e31c66780f4a8c2
PrathameshPalyekar <pratham2604@gmail.com>
2015-10-04 17:16:42 +0530

    files updated

--- JavaScript1.js
+++ JavaScript1.js
@@ -1,1 +1,34 @@
-﻿
+﻿$(document).ready(function () {
+    $(".tintscreen").hide();
+    $("#project1").mouseover(function () {
+        $("#tint1").show();
+    });
+    $("#project1").mouseout(function () {
+        $("#tint1").hide();
+    });
+    $("#project2").mouseover(function () {
+        $("#tint2").show();
+    });
+    $("#project2").mouseout(function () {
+        $("#tint2").hide();
+    });
+    $("#project3").mouseover(function () {
+        $("#tint3").show();
+    });
+    $("#project3").mouseout(function () {
+        $("#tint3").hide();
+    });
+    $("#project4").mouseover(function () {
+        $("#tint4").show();
+    });
+    $("#project4").mouseout(function () {
+        $("#tint4").hide();
+    });
+    $("#project5").mouseover(function () {
+        $("#tint5").show();
+    });
+    $("#project5").mouseout(function () {
+        $("#tint5").hide();
+    });
+});
+
